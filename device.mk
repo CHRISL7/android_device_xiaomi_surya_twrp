d54579e26dbbdd419fdfbafb1c4934a68e02d08a
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-11-21 18:03:16 +0000

    surya: inherit updatable_apex
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@
 # Define hardware platform
 PRODUCT_PLATFORM := sm6150
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
